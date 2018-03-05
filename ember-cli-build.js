250f093de5d82fe416e261ea6e3d890ec460e40d
Mike Wilkerson <mwilkerson@gmail.com>
2018-03-05 11:04:59 -0800

    Initial commit
    * Create this as an ember addon
    * ember install @fortawesome/ember-fontawesome
    * Add an {{fa-icon}} to the dummy app of this repo
    
    See Issue: https://github.com/FortAwesome/ember-fontawesome/issues/18
    See PR: https://github.com/FortAwesome/ember-fontawesome/pull/19

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,11 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
     // Add options here
-  });
+  fontawesome: {
+    icons: {
+      'fontawesome-free-solid': 'all'
+    }
+  }});
 
   /*
     This build file specifies the options for the dummy test app of this
